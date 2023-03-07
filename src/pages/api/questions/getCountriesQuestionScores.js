68ff108d4041a18794608fb8b3510b482ff05aec
Abu Saeed <theyumstar@gmail.com>
2023-03-06 20:39:13 -0500

    Add endpoint for getting quiz scores

---
+++ src/pages/api/questions/getCountriesQuestionScores.js
@@ -0,0 +1,68 @@
+import connectToDB from "@/server/utils/connectDB"
+import jwt from "jsonwebtoken";
+import { QuestionResult } from "@/server/models/QuestionResultModel";
+import { Learner } from "@/server/models/LearnerModel";
+import { getCookie } from "cookies-next";
+import { CountryQuestionList } from "@/server/models/CountryQuestionListModel";
+const handler = async (req, res) => {
+    if(req.method == 'GET'){
+        try{
+            const userToken = getCookie("userToken", {req, res});
+            let userEmail;
+            jwt.verify(userToken, process.env.PRIVATE_KEY, async (err, decoded) => {
+                if(err){
+                    return res.status(400).send({message: "Invalid token"})
+                }
+                const userId = decoded.id;
+                
+                await Learner.findOne({_id: userId}).then((learner) => {
+                    userEmail = learner.email
+                })
+                .catch((error) => {return res.status(400).json({message: "Error authenticating user"}) })
+                var questionResults = await QuestionResult.find({user: userEmail})
+                if(questionResults){
+                    var scores = [];
+                    questionResults.forEach((result) => {
+                        const code = result.question.code;
+                        const country = scores.find((country) =>  country.countryCode == code && typeof country.score == 'number');
+                        if(country){
+                            const index = scores.indexOf(country);
+                            console.log(index)
+                            scores[index] = {...country, score: (country.score + result.result)}
+                        }
+                        else {
+                            scores.push({countryCode: code, score: result.result})
+                        }
+                    })
+                    for(var i = 0; i < scores.length; i++){
+                        const code = scores[i].countryCode;
+                        var questionListLength = ((await CountryQuestionList.findOne({'country.code': code})).list).length
+                        scores[i] = {...scores[i], totalQuestions: questionListLength}
+                    }
+                    // scores.forEach((score) => {
+                    //     const code = score.countryCode;
+                    //     var questionListLength = ((await CountryQuestionList.findOne({'country.code': id})).list).length
+                    // }) 
+                    
+                   res.status(200).send(scores)
+
+                }
+                else {
+                    res.status(500).send({message: 'Error retrieving results for user'})
+                }
+
+                
+            })
+        }
+        catch(error) {
+            res.status(400).send(error.message)
+        }
+   
+        
+    }
+    else {
+        res.status(400).send("Invalid request type")
+    }
+}
+
+export default connectToDB(handler)
