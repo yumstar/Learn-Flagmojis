37b3a3f2ec0c8e1cb9641ca21be42567a69a3558
Abu Saeed <theyumstar@gmail.com>
2023-03-16 23:15:46 -0400

    add': revert copy message component

--- src/components/LearnerInfoContainer.js
+++ src/components/LearnerInfoContainer.js
@@ -7,9 +7,22 @@ import PersonalInfo from "@/components/PersonalInfo";
 import ScoreInfo from "@/components/ScoreInfo";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { useState } from "react";
-import { Tooltip } from 'react-tooltip'
 export default function LearnerInfoContainer({ userDataObj }) {
-
+  const [copyText, setCopyText] = useState({
+    copyMessage: "",
+    copyEmoji: ""
+  });
+  const [showCopyText, setShowCopyText] = useState(false);
+  const onTextCopy = () => {
+    setCopyText(
+      {
+        copyMessage: "Copied!",
+        copyEmoji: "📋"
+      }
+    )
+    setShowCopyText(true);
+    const timeoutId = setTimeout(() => { setShowCopyText(false) }, 2000)
+  }
   return (
     <Box className="learner-page">
       <Box className="learner-info" sx={accountInfoComponentStyles} >
@@ -18,10 +31,13 @@ export default function LearnerInfoContainer({ userDataObj }) {
       <Box className="account-action-buttons" sx={{ width: '100%', textAlign: 'center' }}>
         <Link href="/"><Button variant="primary" sx={{ mr: 3 }}>Back to Flags</Button></Link>
         <Box className="share-link" sx={{ display: "inline" }}>
-          <CopyToClipboard text={`${process.env.NEXT_PUBLIC_HOSTNAME}/learner/${userDataObj.id}`}>
-            <Button variant="primary" data-tooltip-id="copy-tooltip" data-tooltip-content="Copied! 📋">Share Link</Button>
+          <CopyToClipboard text={`${process.env.NEXT_PUBLIC_HOSTNAME}/learner/${userDataObj.id}`} onCopy={onTextCopy}>
+            <Button variant="primary">Share Link</Button>
           </CopyToClipboard>
-          <Tooltip id="copy-tooltip" openOnClick/>
+          <Box className="copy-message" sx={{ display: "inline", mx: 2 }}>
+            <Text sx={{ fontFamily: "body", display: showCopyText ? "inline" : "none" }}>{copyText.copyMessage}</Text>
+            <Text sx={{ fontFamily: "emoji", display: showCopyText ? "inline" : "none" }}>{copyText.copyEmoji}</Text>
+          </Box>
         </Box>
       </Box>
       </Box>
